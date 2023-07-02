b5475a6a9a34448830b1e1caa7432298c0d14edf
bantler <bantler@googlemail.com>
2023-07-02 10:54:31 +0100

    add fix to wsl dsn

--- .zsh/wsl2fix.zsh
+++ .zsh/wsl2fix.zsh
@@ -6,3 +6,7 @@ fix_wsl2_interop() {
         fi
     done
 }
+
+fixdns() {
+  command sudo bash -c "sed -i '/management.azure.com/d' /etc/hosts" ; sudo bash -c 'echo "$(dig management.azure.com | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}$") management.azure.com" >> /etc/hosts'
+}
