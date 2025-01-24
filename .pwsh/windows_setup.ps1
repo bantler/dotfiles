9c961cff24ddce2d899eb79ddfcb333ad6505b2b
Matt Collin <matt.j.collin@gmail.com>
2025-01-24 19:40:06 +0000

    adding windows setup script

---
+++ .pwsh/windows_setup.ps1
@@ -0,0 +1,9 @@
+# Install NerdFonts Module
+Install-PSResource -Name NerdFonts
+Import-Module -Name NerdFonts
+
+#FireCode
+Install-NerdFont -Name 'FiraCode' -Scope CurrentUser
+
+# Hack Nerd Font
+Install-NerdFont -Name 'Hack' -Scope CurrentUser
