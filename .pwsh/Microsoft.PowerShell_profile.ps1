e016bae9f9e0ac1d4998cde10c7dbb3610dad2ce
Matt Collin <matt.j.collin@gmail.com>
2025-01-25 14:45:30 +0000

    adding ssh auto start for powershell

--- .pwsh/Microsoft.PowerShell_profile.ps1
+++ .pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,9 @@
 $ENV:STARSHIP_CONFIG = "$HOME\.config\starship.toml"
 Invoke-Expression (&starship init powershell)
 
+# Ensure ssh service and ssh agent are running
+Start-Service ssh-agent
+ssh-add
+
 # set custom alias'
 Set-Alias -Name tf -Value terraform
