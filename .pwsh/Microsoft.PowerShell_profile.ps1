49a440cc950ac72946aed1ecb0e67cedf7197bfe
Matt Collin <matt.j.collin@gmail.com>
2025-01-25 14:49:22 +0000

    removing powershell auto ssh start

--- .pwsh/Microsoft.PowerShell_profile.ps1
+++ .pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,5 @@
 $ENV:STARSHIP_CONFIG = "$HOME\.config\starship.toml"
 Invoke-Expression (&starship init powershell)
 
-# Ensure ssh service and ssh agent are running
-Start-Service ssh-agent
-ssh-add
-
 # set custom alias'
 Set-Alias -Name tf -Value terraform
