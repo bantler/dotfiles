90cbe970503bcc4607efe1df1d9fea8569f894de
Matt Collin <matt.j.collin@gmail.com>
2025-02-06 17:38:39 +0000

    correcting username

--- setup/wsl/install.sh
+++ setup/wsl/install.sh
@@ -38,33 +38,31 @@ fi
 
 # Prompt for username
 echo "Enter new username"
-read -s new_user </dev/tty
+read -s username </dev/tty
 
 echo "Enter password for $username: "
 read -s password </dev/tty
 
 # Check if the user already exists
-if id "$new_user" &>/dev/null; then
-    echo "User '$new_user' already exists!"
+if id "$username" &>/dev/null; then
+    echo "User '$username' already exists!"
     exit 1
 fi
 
 # Create the user
-sudo useradd -m -s /bin/bash "$username"  
+sudo useradd -m -s /bin/bash "$username"
 
 # Change user password
 echo "$username:$password" | sudo chpasswd
 
 # Prompt for sudo access
-read -p "Grant sudo access to $new_user? (y/n): " sudo_access </dev/tty
+read -p "Grant sudo access to $username? (y/n): " sudo_access </dev/tty
 if [[ $sudo_access == "y" ]]; then
-    sudo usermod -aG sudo "$new_user"
-    echo "$new_user has been added to the sudo group."
+    sudo usermod -aG sudo "$username"
+    echo "$username has been added to the sudo group."
 fi
 
-echo "User $new_user created successfully."
-
-break
+echo "User $username created successfully."
 
 # Create new user
 #sudo adduser bantler
@@ -74,109 +72,109 @@ break
 #echo -e "[user]\ndefault=bantler" | sudo tee -a /etc/wsl.conf > /dev/null
 
 # change user
-su - bantler
+# su - bantler
 
-# touch hushlogin
-touch ~/.hushlogin
+# # touch hushlogin
+# touch ~/.hushlogin
 
-# Create ssh key
-sudo ssh-keygen -t ed25519
+# # Create ssh key
+# sudo ssh-keygen -t ed25519
 
-# Instal yadm and clone dotfiles repo
-sudo apt-get install yadm
-yadm clone git@github.com:bantler/dotfiles.git
-yadm status
+# # Instal yadm and clone dotfiles repo
+# sudo apt-get install yadm
+# yadm clone git@github.com:bantler/dotfiles.git
+# yadm status
 
-# Install zsh shell
-sudo apt-get install zsh
+# # Install zsh shell
+# sudo apt-get install zsh
 
-# Make zsh the default shell
-sudo chsh -s /bin/zsh
+# # Make zsh the default shell
+# sudo chsh -s /bin/zsh
 
-# Install zsh plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-completions.git ~/.zsh/zsh-completions
+# # Install zsh plugins
+# git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+# git clone https://github.com/zsh-users/zsh-completions.git ~/.zsh/zsh-completions
 
-# Update and install dependhancies and remote so
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl lsb-release wget gpg gnupg apt-transport-https software-properties-common apt-transport-https -y
+# # Update and install dependhancies and remote so
+# sudo apt-get update
+# sudo apt-get install -y ca-certificates curl lsb-release wget gpg gnupg apt-transport-https software-properties-common apt-transport-https -y
 
-wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
-sudo dpkg -i packages-microsoft-prod.deb
+# wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
+# sudo dpkg -i packages-microsoft-prod.deb
 
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-rm -f packages.microsoft.gpg
+# wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+# sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+# echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+# rm -f packages.microsoft.gpg
 
-sudo mkdir -p /etc/apt/keyrings
-curl -sLS https://packages.microsoft.com/keys/microsoft.asc |
-gpg --dearmor | sudo tee /etc/apt/keyrings/microsoft.gpg > /dev/null
-sudo chmod go+r /etc/apt/keyrings/microsoft.gpg
+# sudo mkdir -p /etc/apt/keyrings
+# curl -sLS https://packages.microsoft.com/keys/microsoft.asc |
+# gpg --dearmor | sudo tee /etc/apt/keyrings/microsoft.gpg > /dev/null
+# sudo chmod go+r /etc/apt/keyrings/microsoft.gpg
 
-AZ_DIST=$(lsb_release -cs)
-echo "Types: deb
-URIs: https://packages.microsoft.com/repos/azure-cli/
-Suites: ${AZ_DIST}
-Components: main
-Architectures: $(dpkg --print-architecture)
-Signed-by: /etc/apt/keyrings/microsoft.gpg" | sudo tee /etc/apt/sources.list.d/azure-cli.sources
+# AZ_DIST=$(lsb_release -cs)
+# echo "Types: deb
+# URIs: https://packages.microsoft.com/repos/azure-cli/
+# Suites: ${AZ_DIST}
+# Components: main
+# Architectures: $(dpkg --print-architecture)
+# Signed-by: /etc/apt/keyrings/microsoft.gpg" | sudo tee /etc/apt/sources.list.d/azure-cli.sources
 
-wget -O- https://apt.releases.hashicorp.com/gpg | \
-gpg --dearmor | \
-sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+# wget -O- https://apt.releases.hashicorp.com/gpg | \
+# gpg --dearmor | \
+# sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
 
-gpg --no-default-keyring \
---keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
---fingerprint
+# gpg --no-default-keyring \
+# --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+# --fingerprint
 
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-sudo tee /etc/apt/sources.list.d/hashicorp.list
+# echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+# https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+# sudo tee /etc/apt/sources.list.d/hashicorp.list
 
-sudo apt-get update
+# sudo apt-get update
 
-# Install starship
-curl -sS https://starship.rs/install.sh | shell
+# # Install starship
+# curl -sS https://starship.rs/install.sh | shell
 
-# Install vscode
-sudo apt-get install code
+# # Install vscode
+# sudo apt-get install code
 
-# Install powershell
-sudo apt-get install powershell -y
+# # Install powershell
+# sudo apt-get install powershell -y
 
-# Install Python and venv
-sudo apt-get install python3
-sudo apt install python3-venv -y
+# # Install Python and venv
+# sudo apt-get install python3
+# sudo apt install python3-venv -y
 
-sudo apt-get install azure-cli
+# sudo apt-get install azure-cli
 
-# Install Terraform and TFEnv
-sudo apt-get install terraform
-terraform -install-autocomplete
+# # Install Terraform and TFEnv
+# sudo apt-get install terraform
+# terraform -install-autocomplete
 
-git clone https://github.com/tfutils/tfenv.git ~/.tfenv
-echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.zprofile
-sudo ln -s ~/.tfenv/bin/* /usr/local/bin
+# git clone https://github.com/tfutils/tfenv.git ~/.tfenv
+# echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.zprofile
+# sudo ln -s ~/.tfenv/bin/* /usr/local/bin
 
-# Install cmatrix
-sudo apt-get install cmatrix -y
+# # Install cmatrix
+# sudo apt-get install cmatrix -y
 
-# Install bat
-sudo apt-get install bat -y
+# # Install bat
+# sudo apt-get install bat -y
 
-# Install fzf
-sudo apt-get install fzf
+# # Install fzf
+# sudo apt-get install fzf
 
-# Install zoxide
- sudo apt install zoxide
+# # Install zoxide
+#  sudo apt install zoxide
 
- # Install eza
-sudo apt install eza -y
+#  # Install eza
+# sudo apt install eza -y
 
-# Install direnv
-sudo apt-get install direnv
+# # Install direnv
+# sudo apt-get install direnv
 
-# Install jq
-sudo apt-get install jq -y
+# # Install jq
+# sudo apt-get install jq -y
