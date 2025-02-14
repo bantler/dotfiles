44a2b4b77d1813d01eb74b8fd795d7a52346297b
Matt Collin <matt.j.collin@gmail.com>
2025-02-14 16:34:27 +0000

    adding pre-requisite packages

--- setup/wsl/install.sh
+++ setup/wsl/install.sh
@@ -2,9 +2,32 @@
 
 # To install this script run "curl -sS https://raw.githubusercontent.com/bantler/dotfiles/refs/heads/main/setup/wsl/install.sh | sudo bash"
 
-# Update and upgrade
+###################################
+# Prerequisites
+
+# Update the list of packages
 sudo apt-get update
 sudo apt-get upgrade -y
+
+# Install pre-requisite packages.
+sudo apt-get install -y wget apt-transport-https software-properties-common
+
+# Get the version of Ubuntu
+source /etc/os-release
+
+# Download the Microsoft repository keys
+wget -q https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb
+
+# Register the Microsoft repository keys
+sudo dpkg -i packages-microsoft-prod.deb
+
+# Delete the Microsoft repository keys file
+rm packages-microsoft-prod.deb
+
+# Update the list of packages after we added packages.microsoft.com
+sudo apt-get update
+
+###################################
 
 # Check if the script is run as root
 if [[ $EUID -ne 0 ]]; then
@@ -118,45 +141,6 @@ sudo apt-get install powershell -y
 echo "Installing python and venv"
 sudo apt-get install python3
 sudo apt install python3-venv -y
-
-# # Update and install dependhancies and remote so
-# sudo apt-get update
-# sudo apt-get install -y ca-certificates curl lsb-release wget gpg gnupg apt-transport-https software-properties-common apt-transport-https -y
-
-# wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
-# sudo dpkg -i packages-microsoft-prod.deb
-
-# wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-# sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-# echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-# rm -f packages.microsoft.gpg
-
-# sudo mkdir -p /etc/apt/keyrings
-# curl -sLS https://packages.microsoft.com/keys/microsoft.asc |
-# gpg --dearmor | sudo tee /etc/apt/keyrings/microsoft.gpg > /dev/null
-# sudo chmod go+r /etc/apt/keyrings/microsoft.gpg
-
-# AZ_DIST=$(lsb_release -cs)
-# echo "Types: deb
-# URIs: https://packages.microsoft.com/repos/azure-cli/
-# Suites: ${AZ_DIST}
-# Components: main
-# Architectures: $(dpkg --print-architecture)
-# Signed-by: /etc/apt/keyrings/microsoft.gpg" | sudo tee /etc/apt/sources.list.d/azure-cli.sources
-
-# wget -O- https://apt.releases.hashicorp.com/gpg | \
-# gpg --dearmor | \
-# sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
-
-# gpg --no-default-keyring \
-# --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
-# --fingerprint
-
-# echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-# https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-# sudo tee /etc/apt/sources.list.d/hashicorp.list
-
-# sudo apt-get update
 
 # sudo apt-get install azure-cli
 
