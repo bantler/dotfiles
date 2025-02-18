908b5bce804b9b91e93500f5a68afa1b1a303656
Matt Collin <matt.j.collin@gmail.com>
2025-02-18 19:49:21 +0000

    adinng missing tfenv config

--- setup/wsl/install.sh
+++ setup/wsl/install.sh
@@ -150,9 +150,9 @@ echo "Installing Terraform and tfenv"
 sudo apt-get install terraform
 terraform -install-autocomplete
 
-# git clone https://github.com/tfutils/tfenv.git ~/.tfenv
-# echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.zprofile
-# sudo ln -s ~/.tfenv/bin/* /usr/local/bin
+git clone https://github.com/tfutils/tfenv.git ~/.tfenv
+echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.zprofile
+sudo ln -s ~/.tfenv/bin/* /usr/local/bin
 
 # # Install cmatrix
 # sudo apt-get install cmatrix -y
