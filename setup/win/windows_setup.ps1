ad627ea096d82b944e629826ba33db6f59b94c3a
Matt Collin <matt.j.collin@gmail.com>
2026-04-13 21:53:50 +0100

    updating winget config

--- setup/win/windows_setup.ps1
+++ setup/win/windows_setup.ps1
@@ -10,35 +10,43 @@
 # Write-Host "Downloading Winget configuration file"
 # Invoke-WebRequest -Uri https://raw.githubusercontent.com/bantler/dotfiles/refs/heads/main/setup/win/configuration.dsc.dev.yaml -OutFile C:\configuration.dsc.dev.yaml
 
-# Enable Winget configure
-Write-Host "Enabling Winget configure"
-winget configure --Enable
+# Install Windows applications via Winget
+if ((Read-Host "Install Windows applications via Winget? (Y/N)") -notin @('n','N')) {
+    Write-Host "Enabling Winget configure"
+    winget configure --Enable
+    
+    Write-Host "Applying winget configuration"
+    winget configure -f "$(Split-Path -Parent $MyInvocation.MyCommand.Path)\configuration.dev.yaml"
+    
+    Write-Host "Upgrading all packages"
+    winget upgrade --all --accept-source-agreements --accept-package-agreements
+}
 
-# Apply Winget configuration
-Write-Host "Applying winget configuration"
+# Add GnuWin32 to PATH
+$pathToAdd = "C:\Program Files (x86)\GnuWin32\bin"
+$userPath = [Environment]::GetEnvironmentVariable("Path", "User")
 
-Write-Host "Current location: $(Get-Location)"
-pause
-$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-winget configure -f "$scriptDir\configuration.dev.yaml"
+if ($userPath -notlike "*$pathToAdd*") {
+    [Environment]::SetEnvironmentVariable("Path", "$userPath;$pathToAdd", "User")
+}
 
-# Install NerdFonts Module
-Write-Host "Installing Nerd Fonts"
-Install-PSResource -Name NerdFonts
-Import-Module -Name NerdFonts
+# # Install NerdFonts Module
+# Write-Host "Installing Nerd Fonts"
+# Install-PSResource -Name NerdFonts
+# Import-Module -Name NerdFonts
 
-#FireCode
-Write-Host "Installing FireCode font"
-Install-NerdFont -Name 'FiraCode' -Scope CurrentUser
+# #FireCode
+# Write-Host "Installing FireCode font"
+# Install-NerdFont -Name 'FiraCode' -Scope CurrentUser
 
-# Hack Nerd Font
-Write-Host "Installing Hack font"
-Install-NerdFont -Name 'Hack' -Scope CurrentUser
+# # Hack Nerd Font
+# Write-Host "Installing Hack font"
+# Install-NerdFont -Name 'Hack' -Scope CurrentUser
 
-# Config Git
-Write-Host "Configuring Git"
-$username = $env:USERNAME
-$email = (Get-ChildItem "HKCU:\Software\Microsoft\Office\*\Common\Identity" -ErrorAction SilentlyContinue | Get-ItemProperty -Name UserEmail -ErrorAction SilentlyContinue).UserEmail
+# # Config Git
+# Write-Host "Configuring Git"
+# $username = $env:USERNAME
+# $email = (Get-ChildItem 'HKCU:\Software\Microsoft\Office\*\Common\Identity' -ErrorAction SilentlyContinue | Get-ItemProperty -Name UserEmail -ErrorAction SilentlyContinue).UserEmail
 
-git config --global user.email $email
-git config --global user.name $username
+# git config --global user.email $email
+# git config --global user.name $username
